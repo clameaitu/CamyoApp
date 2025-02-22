5e120ae5c9f2e121833712d48da3333bffc9a5e4
Cargarmar18 <93669043+Cargarmar18@users.noreply.github.com>
2025-02-22 16:53:51 +0100

    feat: add backend and user model for testing

---
+++ backend/src/main/java/com/camyo/backend/usuario/UsuarioService.java
@@ -0,0 +1,30 @@
+package com.camyo.backend.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> obtenerUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> obtenerUsuarioPorId(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public Usuario guardarUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void eliminarUsuario(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+}
