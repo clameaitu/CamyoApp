bba26f316fffaf7fddab8f9d0a049d9c010b9638
FranciscoPerezUS <fraperman2@alum.us.es>
2025-02-25 18:36:01 +0100

    feat: add relations for carga and oferta
    Added the Ofertar<- Carga relation in the Carga class
    
    Refs #11

--- backend/src/main/java/com/camyo/backend/oferta/Carga.java
+++ backend/src/main/java/com/camyo/backend/oferta/Carga.java
@@ -1,12 +1,18 @@
 package com.camyo.backend.oferta;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Carga{
@@ -16,12 +22,29 @@ public class Carga{
     private Long id;
 
     String mercancia;
+
     Semirremolque semirremolque;
+
     Double peso;
+
     String Origen;
+
     String Destino;
-    LocalDate inicio;
-    LocalDateTime finMinimo;
-    LocalDateTime finMaximo;
+
     Integer distancia;
+
+    @DateTimeFormat(pattern = "yyyy/MM/dd HH/mm")
+    LocalDateTime inicio;
+
+    @DateTimeFormat(pattern = "yyyy/MM/dd HH/mm")
+    LocalDateTime finMinimo;
+
+    @DateTimeFormat(pattern = "yyyy/MM/dd HH/mm")
+    LocalDateTime finMaximo;
+
+
+    @OneToOne(cascade = { CascadeType.DETACH, CascadeType.REFRESH, CascadeType.PERSIST })
+	@JoinColumn(name = "oferta_id", referencedColumnName = "id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Oferta oferta;
 }
