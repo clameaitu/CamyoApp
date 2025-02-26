b697cd8f28a1d413b2fbb5292bb4b9938473da5d
Cargarort3 <cargarort3@alum.us.es>
2025-02-26 20:13:06 +0100

    feat: add company model
    #14

---
+++ backend/src/main/java/com/camyo/backend/empresa/Empresa.java
@@ -0,0 +1,51 @@
+package com.camyo.backend.empresa;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.validator.constraints.URL;
+
+import com.camyo.backend.usuario.Usuario;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "empresas")
+@Getter
+@Setter
+public class Empresa {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "web")
+	@NotBlank
+    @URL
+	private String web;
+
+    @Column(name = "nif", unique = true)
+    @NotBlank
+    @Pattern(regexp = "^[A-Z]\\d{8}$")
+    private String nif;
+
+    @OneToOne(cascade = { CascadeType.DETACH, CascadeType.REFRESH, CascadeType.PERSIST })
+	@JoinColumn(name = "usuario_id", referencedColumnName = "id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Usuario usuario;
+
+}
