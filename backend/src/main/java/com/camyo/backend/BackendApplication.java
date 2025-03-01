4537c9b0082349fbaa716bed638169ad7e6ea1e3
Adriana Vento Conesa <adrvencon@alum.us.es>
2025-03-01 13:24:23 +0100

    feat: include .env configuration for secret token
    For deployment purposes it's already configured in Github Secrets.

--- backend/src/main/java/com/camyo/backend/BackendApplication.java
+++ backend/src/main/java/com/camyo/backend/BackendApplication.java
@@ -3,10 +3,14 @@ package com.camyo.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("CAMYO_APP_JWT_SECRET", dotenv.get("CAMYO_APP_JWT_SECRET"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
