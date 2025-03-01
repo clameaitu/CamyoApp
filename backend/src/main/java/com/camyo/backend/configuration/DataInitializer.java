377cacf91c2bb8e02c89751f6055ebc3f711e0aa
Adriana Vento Conesa <adrvencon@alum.us.es>
2025-03-01 12:52:26 +0100

    fix: fix duplicate entry error

--- backend/src/main/java/com/camyo/backend/configuration/DataInitializer.java
+++ backend/src/main/java/com/camyo/backend/configuration/DataInitializer.java
@@ -21,8 +21,8 @@ public class DataInitializer {
     public void init() throws SQLException {
         try (Connection connection = dataSource.getConnection()) {
             Statement statement = connection.createStatement();
-            statement.executeUpdate("INSERT INTO authorities (id, authority) VALUES (1, 'CAMIONERO')");
-            statement.executeUpdate("INSERT INTO authorities (id, authority) VALUES (2, 'EMPRESA')");
+            statement.executeUpdate("INSERT IGNORE INTO authorities (id, authority) VALUES (1, 'CAMIONERO')");
+            statement.executeUpdate("INSERT IGNORE INTO authorities (id, authority) VALUES (2, 'EMPRESA')");
         }
     }
 }
