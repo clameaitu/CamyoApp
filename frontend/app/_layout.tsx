eeb62e0edb358f6106c8ddb6b9847b5e33b6816a
Isabel <isacancor@alum.us.es>
2025-03-03 22:55:53 +0100

    feat: add login screen

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout() {
       {/*es el título de la pestaña en el navegador y se ve en la UI, tiene que ser bonito*/}
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" options={{ title: "Inicio" }} />
+        <Stack.Screen name="login" options={{ title: "Iniciar Sesión" }} />
         <Stack.Screen name="registro" options={{ title: "Registro" }} />
         <Stack.Screen name="miperfil" options={{ title: "Mi Perfil Camionero" }} />
         <Stack.Screen name="miperfilempresa" options={{ title: "Mi Perfil Empresa" }} />
