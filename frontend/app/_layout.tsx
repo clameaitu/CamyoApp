0c5a312ce62c94498b62416899343e29ea9e91a2
Joserra24 <josbanbot@alum.us.es>
2025-03-03 17:16:07 +0100

    fix: Change because launching the application did not work

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -7,13 +7,18 @@ export default function RootLayout() {
   const isMobile = Platform.OS === 'ios' || Platform.OS === 'android';
 
   return (
-    <Stack>
-      <Stack.Screen name="/" options={{ headerTitle: "HomeScreen" }} />
-      <Stack.Screen name="/ExampleScreen" options={{ headerTitle: "About" }} />
-      <Stack.Screen name="/CompanyDetails" options={{ headerTitle: "CompanyDetails" }} />
-      <Stack.Screen name="/CompanyProfile" options={{ headerTitle: "CompanyProfile" }} />
-      <Stack.Screen name="/UserProfileScreen" options={{ headerShown: false }} />
+    <>
+      {!isMobile && <CamyoWebNavBar />}
+      {/*name es el nombre que se usa para navegar y hacer router.push(name)*/}
+      {/*es el título de la pestaña en el navegador y se ve en la UI, tiene que ser bonito*/}
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false, title: "Inicio" }} />
+        <Stack.Screen name="/user/MiPerfil" options={{ headerShown: false, title: "Mi Perfil" }} />
+        <Stack.Screen name="/CompanyProfile" options={{ headerShown: false, title: "Mi Perfil" }} />
+        <Stack.Screen name="/CompanyDetails" options={{ headerShown: false, title: "Detalle de empresa" }} />
+      </Stack>
+      {isMobile && <BottomBar />}
+    </>
 
-    </Stack>
   );
 }
