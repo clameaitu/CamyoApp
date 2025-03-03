e3af3702e35bf74a7cf675151ace3a2f01f1eb19
Isabel <isacancor@alum.us.es>
2025-03-03 23:37:44 +0100

    fix: add register screen container

--- frontend/app/registro/index.tsx
+++ frontend/app/registro/index.tsx
@@ -54,216 +54,218 @@ const RegisterScreen = () => {
 
   return (
     <ScrollView style={[globalStyles.container, { paddingTop: 100 }]}>
-      <Text style={globalStyles.title}>Registro de Nuevo Usuario</Text>
+      <View style={globalStyles.formContainer}>
+        <Text style={globalStyles.title}>Registro de Nuevo Usuario</Text>
 
-      <View style={{ borderBottomWidth: 1, borderBottomColor: colors.mediumGray, paddingBottom: 10, marginBottom: 20 }}>
-        <TextInput
-          style={globalStyles.input}
-          placeholder="Nombre"
-          placeholderTextColor={colors.secondary}
-          onChangeText={(value) => handleInputChange("nombre", value)}
-        />
-        <TextInput
-          style={globalStyles.input}
-          placeholder="Apellido"
-          placeholderTextColor={colors.secondary}
-          onChangeText={(value) => handleInputChange("apellido", value)}
-        />
-        <TextInput
-          style={globalStyles.input}
-          placeholder="Email"
-          placeholderTextColor={colors.secondary}
-          keyboardType="email-address"
-          onChangeText={(value) => handleInputChange("email", value)}
-        />
-        <TextInput
-          style={globalStyles.input}
-          placeholder="Contraseña"
-          placeholderTextColor={colors.secondary}
-          secureTextEntry
-          onChangeText={(value) => handleInputChange("password", value)}
-        />
-        <TextInput
-          style={globalStyles.input}
-          placeholder="Número de teléfono"
-          placeholderTextColor={colors.secondary}
-          keyboardType="phone-pad"
-          onChangeText={(value) => handleInputChange("telefono", value)}
-        />
-        <TextInput
-          style={globalStyles.input}
-          placeholder="Localización"
-          placeholderTextColor={colors.secondary}
-          onChangeText={(value) => handleInputChange("localizacion", value)}
-        />
+        <View style={{ borderBottomWidth: 1, borderBottomColor: colors.mediumGray, paddingBottom: 10, marginBottom: 20 }}>
+          <TextInput
+            style={globalStyles.input}
+            placeholder="Nombre"
+            placeholderTextColor={colors.secondary}
+            onChangeText={(value) => handleInputChange("nombre", value)}
+          />
+          <TextInput
+            style={globalStyles.input}
+            placeholder="Apellido"
+            placeholderTextColor={colors.secondary}
+            onChangeText={(value) => handleInputChange("apellido", value)}
+          />
+          <TextInput
+            style={globalStyles.input}
+            placeholder="Email"
+            placeholderTextColor={colors.secondary}
+            keyboardType="email-address"
+            onChangeText={(value) => handleInputChange("email", value)}
+          />
+          <TextInput
+            style={globalStyles.input}
+            placeholder="Contraseña"
+            placeholderTextColor={colors.secondary}
+            secureTextEntry
+            onChangeText={(value) => handleInputChange("password", value)}
+          />
+          <TextInput
+            style={globalStyles.input}
+            placeholder="Número de teléfono"
+            placeholderTextColor={colors.secondary}
+            keyboardType="phone-pad"
+            onChangeText={(value) => handleInputChange("telefono", value)}
+          />
+          <TextInput
+            style={globalStyles.input}
+            placeholder="Localización"
+            placeholderTextColor={colors.secondary}
+            onChangeText={(value) => handleInputChange("localizacion", value)}
+          />
 
-        <TextInput
-          style={[globalStyles.input, { height: 120 }]}
-          placeholder="Descripción (Opcional)"
-          placeholderTextColor={colors.secondary}
-          multiline
-          numberOfLines={3}
-          onChangeText={(value) => handleInputChange("descripcion", value)}
-        />
+          <TextInput
+            style={[globalStyles.input, { height: 120 }]}
+            placeholder="Descripción (Opcional)"
+            placeholderTextColor={colors.secondary}
+            multiline
+            numberOfLines={3}
+            onChangeText={(value) => handleInputChange("descripcion", value)}
+          />
 
-        {/* Selector de Imagen */}
-        <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 20 }}>
-          <TouchableOpacity onPress={pickImage} style={[globalStyles.button, { backgroundColor: colors.lightBlue, flex: 1, marginRight: 5 }]}>
-            {formData.foto && <Text style={globalStyles.buttonText}>Cambiar Foto</Text>}
-            {!formData.foto && <Text style={globalStyles.buttonText}>Seleccionar Foto</Text>}
-            
+          {/* Selector de Imagen */}
+          <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 20 }}>
+            <TouchableOpacity onPress={pickImage} style={[globalStyles.button, { backgroundColor: colors.lightBlue, flex: 1, marginRight: 5 }]}>
+              {formData.foto && <Text style={globalStyles.buttonText}>Cambiar Foto</Text>}
+              {!formData.foto && <Text style={globalStyles.buttonText}>Seleccionar Foto</Text>}
+              
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setFormData({ ...formData, foto: null })} style={[globalStyles.button, { backgroundColor: colors.red, flex: 1, marginLeft: 5 }]}>
+              <Text style={globalStyles.buttonText}>Eliminar Foto</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Mostrar la imagen seleccionada */}
+          {formData.foto && (
+            <Image source={{ uri: formData.foto }} style={{ width: 100, height: 100, borderRadius: 50, marginBottom: 20 }} />
+          )}
+        </View>
+
+        {/* =================== CAMPOS ESPECÍFICOS SEGÚN TIPO DE USUARIO ================= */}
+
+        <Text style={{ fontSize: 20, fontWeight: "bold", color: colors.secondary, textAlign: "center", marginBottom: 12 }}>
+          Selecciona qué tipo de usuario eres:
+        </Text>
+
+        {/* Selector de Usuario */}
+        <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 12 }}>
+          <TouchableOpacity
+            style={[
+              globalStyles.userTypeButton,
+              userType === "camionero" ? globalStyles.selectedUserType : { backgroundColor: colors.lightGray, borderWidth: 1, borderColor: colors.mediumGray },
+            ]}
+            onPress={() => setUserType("camionero")}
+          >
+            <Text style={[globalStyles.userTypeText, { color: userType === "camionero" ? colors.white : colors.secondary, fontSize: 18 }]}>
+              Camionero</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => setFormData({ ...formData, foto: null })} style={[globalStyles.button, { backgroundColor: colors.red, flex: 1, marginLeft: 5 }]}>
-            <Text style={globalStyles.buttonText}>Eliminar Foto</Text>
+          <TouchableOpacity
+            style={[
+              globalStyles.userTypeButton,
+              userType === "empresa" ? globalStyles.selectedUserType : { backgroundColor: colors.lightGray, borderWidth: 1, borderColor: colors.mediumGray },
+            ]}
+            onPress={() => setUserType("empresa")}
+          >
+            <Text style={[globalStyles.userTypeText, { color: userType === "empresa" ? colors.white : colors.secondary, fontSize: 18 }]}>
+              Empresa</Text>
           </TouchableOpacity>
         </View>
 
-        {/* Mostrar la imagen seleccionada */}
-        {formData.foto && (
-          <Image source={{ uri: formData.foto }} style={{ width: 100, height: 100, borderRadius: 50, marginBottom: 20 }} />
+        {/* Campos dinámicos según usuario */}
+        {userType === "camionero" ? (
+          <>
+            <TextInput
+              style={globalStyles.input}
+              placeholder="DNI"
+              placeholderTextColor={colors.secondary}
+              onChangeText={(value) => handleInputChange("dni", value)}
+            />
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Experiencia (años)"
+              placeholderTextColor={colors.secondary}
+              keyboardType="numeric"
+              onChangeText={(value) => handleInputChange("experiencia", value)}
+            />
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Licencia(s) (AM, A1, A2, A, B, C1, C+E, D1+E...)"
+              placeholderTextColor={colors.secondary}
+              onChangeText={(value) => handleInputChange("licencias", value)}
+            />
+
+            <Text style={{ color: colors.secondary, fontSize: 16, marginRight: 10, flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
+              Disponibilidad:</Text>
+            <Selector 
+              value={formData.disponibilidad} 
+              onChange={(value) => handleInputChange("disponibilidad", value)} 
+              options={["Nacional", "Internacional"]} 
+              colors={colors} 
+              globalStyles={globalStyles} 
+            />
+
+
+
+
+            {/* CAP */}
+            <Text style={{ color: colors.secondary, fontSize: 16, marginRight: 10, flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
+              ¿Tiene CAP (Certificado de Aptitud Profesional)?:</Text>
+            <BooleanSelector 
+              value={formData.tieneCAP} 
+              onChange={(value) => handleInputChange("tieneCAP", value)} 
+              colors={colors} 
+              globalStyles={globalStyles} 
+            />
+
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Fecha de expiración del CAP (DD-MM-YYYY)"
+              placeholderTextColor={colors.secondary}
+              onChangeText={(value) => handleInputChange("expiracionCAP", value)}
+            />
+
+            <Text style={{ color: colors.secondary, fontSize: 16, marginRight: 10, flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
+              ¿Tiene camión propio?</Text>
+            <BooleanSelector 
+              value={formData.vehiculoPropio} 
+              onChange={(value) => handleInputChange("vehiculoPropio", value)} 
+              colors={colors} 
+              globalStyles={globalStyles} 
+            />
+          </>
+
+
+        ) : (
+
+
+          <>
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Nombre de la empresa"
+              placeholderTextColor={colors.secondary}
+              onChangeText={(value) => handleInputChange("empresaNombre", value)}
+            />
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Página web (Opcional)"
+              placeholderTextColor={colors.secondary}
+              onChangeText={(value) => handleInputChange("web", value)}
+            />
+            
+            <Text style={{ color: colors.secondary, fontSize: 16, marginRight: 10, flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
+              Tipo de Identificación:</Text>
+            <Selector 
+              value={formData.tipoId} 
+              onChange={(value) => handleInputChange("tipoId", value)} 
+              options={["NIF", "CIF", "NIE", "REAT"]} 
+              colors={colors} 
+              globalStyles={globalStyles} 
+            />
+
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Número de Identificación"
+              placeholderTextColor={colors.secondary}
+              keyboardType="numeric"
+              onChangeText={(value) => handleInputChange("numId", value)}
+            />
+          </>
         )}
-      </View>
 
-      {/* =================== CAMPOS ESPECÍFICOS SEGÚN TIPO DE USUARIO ================= */}
-
-      <Text style={{ fontSize: 20, fontWeight: "bold", color: colors.secondary, textAlign: "center", marginBottom: 12 }}>
-        Selecciona qué tipo de usuario eres:
-      </Text>
-
-      {/* Selector de Usuario */}
-      <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 12 }}>
-        <TouchableOpacity
+        {/* Botón de registro */}
+        <TouchableOpacity 
           style={[
-            globalStyles.userTypeButton,
-            userType === "camionero" ? globalStyles.selectedUserType : { backgroundColor: colors.lightGray, borderWidth: 1, borderColor: colors.mediumGray },
+            globalStyles.button, 
+            { borderRadius: 12, elevation: 5 }
           ]}
-          onPress={() => setUserType("camionero")}
         >
-          <Text style={[globalStyles.userTypeText, { color: userType === "camionero" ? colors.white : colors.secondary, fontSize: 18 }]}>
-            Camionero</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[
-            globalStyles.userTypeButton,
-            userType === "empresa" ? globalStyles.selectedUserType : { backgroundColor: colors.lightGray, borderWidth: 1, borderColor: colors.mediumGray },
-          ]}
-          onPress={() => setUserType("empresa")}
-        >
-          <Text style={[globalStyles.userTypeText, { color: userType === "empresa" ? colors.white : colors.secondary, fontSize: 18 }]}>
-            Empresa</Text>
+          <Text style={[globalStyles.buttonText, { fontSize: 30 }]}>Registrarse</Text>
         </TouchableOpacity>
       </View>
-
-      {/* Campos dinámicos según usuario */}
-      {userType === "camionero" ? (
-        <>
-          <TextInput
-            style={globalStyles.input}
-            placeholder="DNI"
-            placeholderTextColor={colors.secondary}
-            onChangeText={(value) => handleInputChange("dni", value)}
-          />
-          <TextInput
-            style={globalStyles.input}
-            placeholder="Experiencia (años)"
-            placeholderTextColor={colors.secondary}
-            keyboardType="numeric"
-            onChangeText={(value) => handleInputChange("experiencia", value)}
-          />
-          <TextInput
-            style={globalStyles.input}
-            placeholder="Licencia(s) (AM, A1, A2, A, B, C1, C+E, D1+E...)"
-            placeholderTextColor={colors.secondary}
-            onChangeText={(value) => handleInputChange("licencias", value)}
-          />
-
-          <Text style={{ color: colors.secondary, fontSize: 16, marginRight: 10, flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
-            Disponibilidad:</Text>
-          <Selector 
-            value={formData.disponibilidad} 
-            onChange={(value) => handleInputChange("disponibilidad", value)} 
-            options={["Nacional", "Internacional"]} 
-            colors={colors} 
-            globalStyles={globalStyles} 
-          />
-
-
-
-
-          {/* CAP */}
-          <Text style={{ color: colors.secondary, fontSize: 16, marginRight: 10, flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
-            ¿Tiene CAP (Certificado de Aptitud Profesional)?:</Text>
-          <BooleanSelector 
-            value={formData.tieneCAP} 
-            onChange={(value) => handleInputChange("tieneCAP", value)} 
-            colors={colors} 
-            globalStyles={globalStyles} 
-          />
-
-          <TextInput
-            style={globalStyles.input}
-            placeholder="Fecha de expiración del CAP (DD-MM-YYYY)"
-            placeholderTextColor={colors.secondary}
-            onChangeText={(value) => handleInputChange("expiracionCAP", value)}
-          />
-
-          <Text style={{ color: colors.secondary, fontSize: 16, marginRight: 10, flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
-            ¿Tiene camión propio?</Text>
-          <BooleanSelector 
-            value={formData.vehiculoPropio} 
-            onChange={(value) => handleInputChange("vehiculoPropio", value)} 
-            colors={colors} 
-            globalStyles={globalStyles} 
-          />
-        </>
-
-
-      ) : (
-
-
-        <>
-          <TextInput
-            style={globalStyles.input}
-            placeholder="Nombre de la empresa"
-            placeholderTextColor={colors.secondary}
-            onChangeText={(value) => handleInputChange("empresaNombre", value)}
-          />
-          <TextInput
-            style={globalStyles.input}
-            placeholder="Página web (Opcional)"
-            placeholderTextColor={colors.secondary}
-            onChangeText={(value) => handleInputChange("web", value)}
-          />
-          
-          <Text style={{ color: colors.secondary, fontSize: 16, marginRight: 10, flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
-            Tipo de Identificación:</Text>
-          <Selector 
-            value={formData.tipoId} 
-            onChange={(value) => handleInputChange("tipoId", value)} 
-            options={["NIF", "CIF", "NIE", "REAT"]} 
-            colors={colors} 
-            globalStyles={globalStyles} 
-          />
-
-          <TextInput
-            style={globalStyles.input}
-            placeholder="Número de Identificación"
-            placeholderTextColor={colors.secondary}
-            keyboardType="numeric"
-            onChangeText={(value) => handleInputChange("numId", value)}
-          />
-        </>
-      )}
-
-      {/* Botón de registro */}
-      <TouchableOpacity 
-        style={[
-          globalStyles.button, 
-          { marginBottom: 80, borderRadius: 12, elevation: 5 }
-        ]}
-      >
-        <Text style={[globalStyles.buttonText, { fontSize: 30 }]}>Registrarse</Text>
-      </TouchableOpacity>
     </ScrollView>
   );
 };
