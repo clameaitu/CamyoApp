a9913596b8fa006bbe8c19c0d39fc76d9e4285d9
Isabel <isacancor@alum.us.es>
2025-03-12 18:41:24 +0100

    feat: el registro funcionaaaa :D (en principio)

--- frontend/app/registro/camionero.tsx
+++ frontend/app/registro/camionero.tsx
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, ScrollView, StyleSheet, Image } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, ScrollView, StyleSheet, Image, Modal } from "react-native";
 import globalStyles from "../../assets/styles/globalStyles";
 import colors from "../../assets/styles/colors";
 import BooleanSelector from "../_components/BooleanSelector";
 import Selector from "../_components/Selector";
 import MultiSelector from "../_components/MultiSelector";
 import { FontAwesome5, MaterialIcons, Entypo } from "@expo/vector-icons";
-import * as ImagePicker from "expo-image-picker";
-import defaultProfileImage from "../../assets/images/react-logo.png";
+import defaultProfileImage from "../../assets/images/defaultAvatar.png";
+import axios from 'axios';
+import { useAuth } from "../../contexts/AuthContext";
+import { useRouter } from "expo-router";
+import { pickImageAsync } from "../../utils/imageUtils";
+
+const BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
+const licencias = ["AM", "A1", "A2", "A", "B", "C1", "C", "C1+E", "C+E", "D1", "D+E","D1+E","D"];
+const licencias_backend = ["AM", "A1", "A2", "A", "B", "C1", "C", "C1_E", "C_E", "D1", "D_E","D1_E","D"];
 
 const CamioneroScreen = () => {
+  const router = useRouter();
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const { login } = useAuth();
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const [formData, setFormData] = useState({
     nombre: "",
@@ -19,17 +30,17 @@ const CamioneroScreen = () => {
     password: "",
     telefono: "",
     localizacion: "",
-    descripcion: "", // opcional
-    foto: null, // opcional, pero recomendable
+    descripcion: "",
+    foto: null,
 
     // Camionero
     dni: "",
     licencias: [],
     disponibilidad: "",
     experiencia: 0,
-    tieneCAP: null,
+    tieneCAP: false,
     expiracionCAP: "",
-    esAutonomo: null,
+    isAutonomo: false,
     tarjetas: []
   });
 
@@ -37,19 +48,73 @@ const CamioneroScreen = () => {
     setFormData((prevState) => ({ ...prevState, [field]: value }));
   };
 
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 1,
-      base64: true,  // Poner la imagen en base64
-    });
-  
-    if (!result.canceled) {
-      const base64Image = `data:image/jpeg;base64,${result.assets[0].base64}`;
+  const handlePickImage = async () => {
+    const base64Image = await pickImageAsync();
+    if (base64Image) {
       setFormData((prevState) => ({ ...prevState, foto: base64Image }));
     }
+  };
+
+  const handleRegister = async () => {
+    const licenciasBackend = formData.licencias.map((licencia) => licencias_backend[licencias.indexOf(licencia)]);
+
+    // Datos del usuario
+    const userData = {
+      nombre: formData.nombre,
+      telefono: formData.telefono,
+      username: formData.username,
+      email: formData.email,
+      localizacion: formData.localizacion,
+      descripcion: formData.descripcion,
+      foto: formData.foto ? formData.foto : "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAwAB/57T3goAAAAASUVORK5CYII=",
+      password: formData.password,
+
+      dni: formData.dni,
+      licencias: licenciasBackend,
+      disponibilidad: formData.disponibilidad,
+      experiencia: parseInt(formData.experiencia.toString()),
+      tieneCAP: formData.tieneCAP,
+      expiracionCAP: formData.expiracionCAP.replace(/(\d{2})-(\d{2})-(\d{4})/, '$3-$2-$1'),
+      isAutonomo: formData.isAutonomo,
+      tarjetas: formData.tarjetas
+    };
+
+    try {
+      const response = await axios.post(`${BACKEND_URL}/auth/signup/camionero`, userData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.status === 200) {
+        console.log('Camionero registrada correctamente', response.data.message);
+        setErrorMessage("")
+      } else {
+        console.error('Error al registrar el camionero/a', response);
+      }
+
+      const responseLogin = await axios.post(`${BACKEND_URL}/auth/signin`, {
+        username: userData.username,
+        password: userData.password,
+      });
+
+      const { token } = responseLogin.data;
+      login(responseLogin.data, token);
+
+      setIsModalVisible(true);
+        setTimeout(() => {
+          router.replace("/");
+        }, 3000);
+      
+    } catch (error) {
+      console.error('Error en la solicitud', error);
+      if (axios.isAxiosError(error)) {
+        setErrorMessage("Los datos introducidos no son correctos. Por favor, compruébalos e inténtalo de nuevo.");
+      } else {
+        setErrorMessage('Error desconocido');
+      }
+    }
+      
   };
 
   // Render input function
@@ -103,13 +168,13 @@ const CamioneroScreen = () => {
             />
 
             {!formData.foto ? (
-              <TouchableOpacity onPress={pickImage} style={[globalStyles.button, { backgroundColor: colors.secondary, flexDirection: "row", alignItems: "center", justifyContent: "center", width: 140, paddingHorizontal: 15 }]}>
+              <TouchableOpacity onPress={handlePickImage} style={[globalStyles.button, { backgroundColor: colors.secondary, flexDirection: "row", alignItems: "center", justifyContent: "center", width: 140, paddingHorizontal: 15 }]}>
                 <MaterialIcons name="add-a-photo" size={20} color={colors.white} style={{ marginRight: 8 }} />
                 <Text style={globalStyles.buttonText}>Añadir Foto</Text>
               </TouchableOpacity>
             ) : (
               <View style={{ flexDirection: "row" }}>
-                <TouchableOpacity onPress={pickImage} style={[globalStyles.button, { backgroundColor: colors.green, marginRight: 7, flexDirection: "row", alignItems: "center", justifyContent: "center", width: 120, paddingHorizontal: 15 }]}>
+                <TouchableOpacity onPress={handlePickImage} style={[globalStyles.button, { backgroundColor: colors.green, marginRight: 7, flexDirection: "row", alignItems: "center", justifyContent: "center", width: 120, paddingHorizontal: 15 }]}>
                   <MaterialIcons name="cached" size={20} color={colors.white} style={{ marginRight: 8 }} />
                   <Text style={globalStyles.buttonText}>Cambiar</Text>
                 </TouchableOpacity>
@@ -123,17 +188,16 @@ const CamioneroScreen = () => {
           </View>
 
           {/* Campos del formulario */}
-          {renderInput("Nombre", "nombre", <FontAwesome5 name="user" size={20} color={colors.primary} />)}
+          {renderInput("Nombre y Apellidos", "nombre", <FontAwesome5 name="user" size={20} color={colors.primary} />)}
           {renderInput("Nombre de Usuario", "username", <FontAwesome5 name="user-alt" size={20} color={colors.primary} />)}
           {renderInput("Email", "email", <MaterialIcons name="email" size={20} color={colors.primary} />, "email-address")}
           {renderPasswordInput()}
           {renderInput("Teléfono", "telefono", <MaterialIcons name="phone" size={20} color={colors.primary} />, "phone-pad")}
           {renderInput("Localización", "localizacion", <MaterialIcons name="location-pin" size={20} color={colors.primary} />)}
-          {renderInput("Descripción (Opcional)", "descripcion", <FontAwesome5 name="align-left" size={20} color={colors.primary} />, "default", false, true)}
+          {renderInput("Descripción", "descripcion", <FontAwesome5 name="align-left" size={20} color={colors.primary} />, "default", false, true)}
 
           {/* Campos del formulario específicos de camionero */}
           {renderInput("DNI", "dni", <FontAwesome5 name="address-card" size={20} color={colors.primary} />)}
-          {renderInput("Licencia(s)", "licencias", <FontAwesome5 name="id-badge" size={20} color={colors.primary} />)}
           <View style={styles.inputContainer}>
               <Text style={{ color: colors.secondary, fontSize: 16, marginRight: 10, flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
                 Selecciona tus licencias:
@@ -141,7 +205,7 @@ const CamioneroScreen = () => {
               <MultiSelector 
                 value={formData.licencias}
                 onChange={(value) => handleInputChange("licencias", value)}
-                options={["C1","C","C1+E","C+E","D1","D+E","E","D"]}
+                options={licencias}
                 colors={colors} 
               />
             </View>
@@ -153,7 +217,7 @@ const CamioneroScreen = () => {
             <Selector 
               value={formData.disponibilidad} 
               onChange={(value) => handleInputChange("disponibilidad", value)} 
-              options={["Nacional", "Internacional"]} 
+              options={["NACIONAL", "INTERNACIONAL"]} 
               colors={colors} 
               globalStyles={globalStyles} 
             />
@@ -174,8 +238,13 @@ const CamioneroScreen = () => {
             />
           </View>
 
-          {/* Expiración del CAP */}
-          {renderInput("Fecha de expiración del CAP", "expiracionCAP", <FontAwesome5 name="calendar" size={20} color={colors.primary} />, "default")}
+          {/* Mostrar campo de tarjetas solo si tiene CAP */}
+          {formData.tieneCAP && (
+            <>
+              {/* Expiración del CAP */}
+              {renderInput("Fecha de expiración del CAP (DD-MM-YYYY)", "expiracionCAP", <FontAwesome5 name="calendar" size={20} color={colors.primary} />, "default")}
+            </>
+          )}
 
           {/* Es autónomo/a? */}
           <View style={styles.inputContainer}>
@@ -183,15 +252,15 @@ const CamioneroScreen = () => {
               ¿Eres autónomo/a?:
             </Text>
             <BooleanSelector 
-              value={formData.esAutonomo} 
-              onChange={(value) => handleInputChange("esAutonomo", value)} 
+              value={formData.isAutonomo} 
+              onChange={(value) => handleInputChange("isAutonomo", value)} 
               colors={colors} 
               globalStyles={globalStyles} 
             />
           </View>
 
           {/* Mostrar campo de tarjetas solo si es autónomo */}
-          {formData.esAutonomo && (
+          {formData.isAutonomo && (
             <View style={styles.inputContainer}>
               <Text style={{ color: colors.secondary, fontSize: 16, marginRight: 10, flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
                 Selecciona tus tarjetas:
@@ -205,10 +274,37 @@ const CamioneroScreen = () => {
             </View>
           )}
 
+          {errorMessage ? (
+            <Text style={{ color: "red", fontSize: 18, marginBottom: 10, justifyContent: "center", textAlign: "center" }}>
+              {errorMessage}
+            </Text>
+          ) : null}
+
           {/* Botón de registro */}
-          <TouchableOpacity style={[globalStyles.button, { width: "100%", borderRadius: 12, elevation: 5 }]}>
+          <TouchableOpacity style={[globalStyles.button, { width: "100%", borderRadius: 12, elevation: 5 }]}
+            onPress={handleRegister}
+          >
             <Text style={[globalStyles.buttonText, { fontSize: 25 }]}>Registrarse</Text>
           </TouchableOpacity>
+
+          {/* Modal de éxito */}
+          <Modal
+            animationType="fade"
+            transparent={true}
+            visible={isModalVisible}
+            onRequestClose={() => setIsModalVisible(false)}
+          >
+            <View style={styles.modalOverlay}>
+              <View style={styles.modalContainer}>
+                {/* Icono del tic verde */}
+                <FontAwesome5 name="check-circle" size={50} color="white" style={styles.modalIcon} />
+                
+                <Text style={styles.modalText}>¡Registro Exitoso!</Text>
+                <Text style={styles.modalText}>Redirigiendo...</Text>
+              </View>
+            </View>
+          </Modal>
+
         </View>
       </View>
     </ScrollView>
@@ -288,6 +384,27 @@ const styles = StyleSheet.create({
   buttonText: {
     color: colors.white,
     fontSize: 16,
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContainer: {
+    backgroundColor: colors.green,
+    padding: 20,
+    borderRadius: 10,
+    width: 250,
+    alignItems: "center",
+  },
+  modalIcon: {
+    marginBottom: 10,
+  },
+  modalText: {
+    fontSize: 18,
+    color: "white",
+    textAlign: "center",
   },
 });
 
